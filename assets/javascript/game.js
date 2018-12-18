aa6c0e12b17dfa6c4a6957fdfec68756aa82a56c
Nicole Wilsey Starr <Nicole.WilseyStarr@gmail.com>
2018-12-18 16:54:32 -0600

    Logic almost complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,79 @@
-//Main javaScript goes here 
+//Main javaScript goes here
 
-//document ready function 
-$(document).ready(function() { 
- 
-var wins = 0;
-console.log(wins);
-    $("#wins").html("Wins: " + wins);
+//document ready function
+$(document).ready(function() {
 
-var losses = 0; 
-console.log(losses);
-$("#losses").html("Losses: " + losses);
-
-var score = 0;
-console.log(score);
-    $("#score").html("Score: " + score);
-
-var compPick = [];
-
-
-reset()
-
-//RESET FUNCTION==========================================
-function reset() {
+    var wins = 0;
+        $("#wins").html("Wins: " + wins);
     
-    compPick = 0;
-
-    //compPick function again with the same code
-    compPick = Math.floor(Math.random() * 120)+ 19;
-        console.log("Computer Pick: " + compPick);
-
-    // gem functions again with the same code
-    //var = gem 1 random number function 1-12 +1 (so as to not get zero) use math.random floor to get score
-    var gem1 = Math.floor(Math.random()*12)+1;
-    console.log("Gem 1: " + gem1);
-    $("#gem-1").attr("value", gem1);
-
-    //var = gem 2 random number function 1-12 +1 (so as to not get zero) use math.random floor to get score 
-    var gem2 = Math.floor(Math.random()*12)+1;
-    console.log("Gem 2: " + gem2);
-    $("#gem-2").attr("value", gem2);
-
-    //var = gem 3 random number function 1-12 +1 (so as to not get zero) use math.random floor to get score
-    var gem3 = Math.floor(Math.random()*12)+1;
-    console.log("Gem 3: " + gem3);
-    $("#gem-3").attr("value", gem3);
-
-    //var = gem 4 random number function 1-12 +1 (so as to not get zero) use math.random floor to get score 
-    var gem4 = Math.floor(Math.random()*12)+1;
-    console.log("Gem 4: " + gem4);
-    $("#gem-4").attr("value", gem4);
-
-//RESET FUNCTION==========================================
-    }
-
-    //on click event calling the img tag
-    $("img").on("click", function() {
-        //new var totalScore = score parsentInt .this function
-        score = score += parseInt($(this).attr("value")); 
-            console.log("total score: " + score);
+    var losses = 0;
+        $("#losses").html("Losses: " + losses);
     
-        //if else statement 
-        // if the score is === to computer pick they win  
-        if (score == compPick) {
-            wins ++;
-            console.log("Wins " + wins);
-            reset();
-        } 
-        // else if the score is > the computer pick they lose
-        else if (score > compPick) {
-            losses ++;
-            console.log("Losses "+ losses);
-            reset();
-        }
-        
-        });
-});
+    var score = [];
+        $("#score").html("Score: 0");
+    
+    var compPick = [];
+    
+    reset()
+    
+    //RESET FUNCTION==========================================
+    
+    function reset() {
+       score = 0; 
+       $("#score").html("Score: 0");
+       
+       compPick = 0;
+
+       //compPick function with math.floor + 19 to get a number between 19 and 120
+       compPick = Math.floor(Math.random() * (120-19))+ 19;
+           console.log("Computer Pick: " + compPick);
+           $("#comp-pick").html("Number to guess: " + compPick);
+    
+       // gem functions again with the same code
+       //var = gem 1 random number function 1-12 +1 (so as to not get zero) use math.random floor to get score
+       var gem1 = Math.floor(Math.random()*12)+1;
+            console.log("Gem 1: " + gem1);
+            $("#gem-1").attr("value", gem1);
+    
+       //var = gem 2 random number function 1-12 +1 (so as to not get zero) use math.random floor to get score
+       var gem2 = Math.floor(Math.random()*12)+1;
+            console.log("Gem 2: " + gem2);
+            $("#gem-2").attr("value", gem2);
+    
+       //var = gem 3 random number function 1-12 +1 (so as to not get zero) use math.random floor to get score
+       var gem3 = Math.floor(Math.random()*12)+1;
+            console.log("Gem 3: " + gem3);
+            $("#gem-3").attr("value", gem3);
+    
+       //var = gem 4 random number function 1-12 +1 (so as to not get zero) use math.random floor to get score
+       var gem4 = Math.floor(Math.random()*12)+1;
+            console.log("Gem 4: " + gem4);
+            $("#gem-4").attr("value", gem4);
+    
+        }   
+    //==========================================RESET FUNCTION
+    
+    
+       //on click event calling the img tag
+       $("img").on("click", function() {
+
+           //new var totalScore = score parsentInt .this function
+           score = score += parseInt($(this).attr("value"));
+           $("#score").html("Score: " + score);
+
+           //if else statement
+           if (score == compPick) {
+               alert("YOU WIN!");
+               wins ++;
+               $("#wins").html("Wins: " + wins);
+               reset(); 
+                 
+           } else if (score > compPick) {
+               alert("YOU LOSE!");
+               losses ++;
+               $("#losses").html("Losses: " + losses);
+               reset();
+            }
+    
+           });
+    });
